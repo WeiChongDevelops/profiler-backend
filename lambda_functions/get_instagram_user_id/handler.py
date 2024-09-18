bba1b591e73b9080c2fd49eeadd081a819c057f2
Wei <wac.weichong@gmail.com>
2024-09-18 17:00:31 +0800

    Adjust get instagram userid to use http body

--- lambda_functions/get_instagram_user_id/handler.py
+++ lambda_functions/get_instagram_user_id/handler.py
@@ -2,7 +2,8 @@ import json
 
 
 def lambda_handler(event, context):
-    username = event["username"]
+    body = json.loads(event['body'])
+    username = body.get('username', '')
     user_id = "sample_user_id"
     print(f"User ID for {username}: {user_id}")
     return {"user_id": user_id}
